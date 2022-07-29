c2e36c6d3f943143ec48c38cf3937bd55140a2bc
Ahmed Messaoud <ahbenmes@microsoft.com>
2022-07-29 06:43:23 -0700

    wip

--- src/tests/e2etest/datasource/LocalDataSource.cs
+++ src/tests/e2etest/datasource/LocalDataSource.cs
@@ -51,7 +51,7 @@ namespace E2eTesting
             }
             Console.Write("[LocalDataSource] Final JSON: " + local);
             File.WriteAllText(_desiredPath, local.ToString());
-            System.Threading.Thread.Sleep(10000);
+            System.Threading.Thread.Sleep(300000);
             return true;
         }
 
