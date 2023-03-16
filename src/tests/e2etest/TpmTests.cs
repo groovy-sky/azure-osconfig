5a72b794df2d1687b92fc0ae0b9659856dd51bc0
Robert Schaedler <rschaedler@microsoft.com>
2023-03-16 19:32:55 +0000

    test

--- src/tests/e2etest/TpmTests.cs
+++ src/tests/e2etest/TpmTests.cs
@@ -31,9 +31,9 @@ namespace E2eTesting
         }
 
         [Test]
-        public void TpmTest_Get()
+        public async Task TpmTest_Get()
         {
-            Tpm reported = GetReported<Tpm>(_componentName);
+            Tpm reported = await GetReported<Tpm>(_componentName);
 
             Assert.Multiple(() =>
             {
